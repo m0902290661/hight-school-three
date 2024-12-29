cbd127d027088a11bffc16e6be12740f51d7b564
m0902290661 <144592540+m0902290661@users.noreply.github.com>
2024-12-29 12:27:03 +0800

    file deil

--- find.js
+++
@@ -1,7 +0,0 @@
-function find(){
-    const finder = document.getElementById('finder').value;
-    const adv = document.getElementById('adv').value;
-    const href = finder + adv;
-    document.getElementById('output').innerHTML=href;
-    location.href=href;
-}
